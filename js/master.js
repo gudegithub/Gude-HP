bfaebf52ee9dc6c587b706576260ea66b22106b0
Kishikawa1286 <k.ishikawa.gude@gmail.com>
2020-03-12 21:01:14 +0900

    first commit

---
+++ js/master.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+  arrows: false,
+  variableWidth: true,
+  infinite: false,
+  focusOnSelect: true,
+  draggable: true,
+  swipe: true,
+  touchThreshold: 70,
+  accessibility: true,
+});
